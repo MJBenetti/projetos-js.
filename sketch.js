5d2e1d899ca9707f0a2078a119e7991c3031ad85
MJBenetti <160861791+MJBenetti@users.noreply.github.com>
2024-08-01 12:20:00 -0300

    adicionando arquivos js e css

---
+++ sketch.js
@@ -0,0 +1,35 @@
+let cor;
+let circleX; //horizontal
+let circleY; //vertical
+
+function setup() {
+  createCanvas(400, 400); // width x height
+  background("rgb(165,89,51)");
+  cor = color(random(0, 255), random(0, 255), random(0, 255));
+  circleX = [0, 0, 0];
+  circleY = [random(height), random(height), random(height)];
+}
+
+//circleX =0,0,0
+//circleY =300,150,50
+
+function draw() {
+  fill(cor);
+  
+  //console.log (circleX.length);
+  for(let contador in circleX){
+    //console.log(contador);
+  circle(circleX[contador], circleY[contador], 50);
+  circleX[contador] += random(0, 3);
+  circleY[contador] += random(-3, 3);
+    
+    if (circleX[contador] >= width) {
+      circleX[contador] = 0;
+      circleY[contador] = random (height);
+    }
+}
+
+  if (mouseIsPressed) {
+    cor = color(random(0, 255), random(0, 255), random(0, 255), random(0, 100));
+  }
+}
